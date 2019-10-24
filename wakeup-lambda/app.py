307f8f1e1bef69e9a6137c867e97942bc76f474e
Sigurður Skúli Sigurgeirsson <siggiskuli@gmail.com>
2019-10-24 19:16:03 +0000

    tiny update

--- wakeup-lambda/app.py
+++ wakeup-lambda/app.py
@@ -6,10 +6,10 @@ app = Chalice(app_name='wakeup-lambda')
 lambda_client = boto3.client('lambda')
 
 LAMBDAS_TO_WAKEUP = [
-	'https://hkgb1tvfwd.execute-api.eu-west-1.amazonaws.com/api/'
+    'https://hkgb1tvfwd.execute-api.eu-west-1.amazonaws.com/api/'
 ]
 LAMBDAS_TO_INVOKE = [
-	'athena-play_intent-lambda-dev-index',
+    'athena-play_intent-lambda-dev-index',
     'athena-increase_volume_intent-lambda-dev-index',
     'athena-resume_intent-lambda-dev-index',
     'athena-decrease_volume_intent-lambda-dev-index',
@@ -25,33 +25,12 @@ LAMBDAS_TO_INVOKE = [
 @app.lambda_function()
 def index(event, context):
     for lambda_url in LAMBDAS_TO_WAKEUP:
-    	requests.post(lambda_url, data=[])
+        requests.post(lambda_url, data=[])
     for lambda_name in LAMBDAS_TO_INVOKE:
-    	response = lambda_client.invoke(
+        response = lambda_client.invoke(
             FunctionName=lambda_name,
             InvocationType='Event',
             LogType='None',
             Payload='',
             Qualifier='$LATEST'
         )
-
-
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
